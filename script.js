18b691fb99e9e7fe1e8512e6f8d6b03a9ed32e43
Bharaath R <bharaathravechandran@gmail.com>
2025-06-10 11:32:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ window.onscroll = () => {
     }
 }
 
-let countDate = new Date('sep 1, 2025 00:00:00').getTime();
+let countDate = new Date('september 1, 2025 00:00:00').getTime();
 
 function CountDown() {
     let now = new Date().getTime();
